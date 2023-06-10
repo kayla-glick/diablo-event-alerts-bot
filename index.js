f3c83edeb3dc734c29a2e5144e40cd89a0818831
Kayla Glick <kaylaglick12@gmail.com>
2023-06-09 20:40:52 -0400

    update script

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { _ } = require('lodash')
 const { parse } = require('node-html-parser')
 const { Client, GatewayIntentBits } = require('discord.js')
 require('dotenv/config')
@@ -11,47 +12,46 @@ const client = new Client({
 })
 
 const getBossTimers = async () => {
-  const response = await fetch('https://diablo4.life/trackers/world-bosses')
-    .then((response) => {
-      const reader = response.body.getReader();
-      return new ReadableStream({
-        start(controller) {
-          return pump();
-          function pump() {
-            return reader.read().then(({ done, value }) => {
-              // When no more data needs to be consumed, close the stream
-              if (done) {
-                controller.close();
-                return;
-              }
-              // Enqueue the next data chunk into our target stream
-              controller.enqueue(value);
-              return pump();
-            });
-          }
-        },
-      });
-    })
-    // Create a new response out of the stream
-    .then((stream) => new Response(stream))
-    .then((response) => response.text())
-  
-  const root = parse(response)
-  root.querySelectorAll('[class*="EventCountdown_content"]')
-  return('Sorry! I\'m not ready yet.')
+  const response = await fetch('https://diablo4.life/api/trackers/world-boss/list')
+  const jsonData = await response.json()
+
+  return jsonData.event
+}
+
+const formatMessage = (event) => {
+  const bossName = event.name
+  const startTime = new Date(event.time);
+  const location = event.location
+  const formattedTime = startTime.toLocaleTimeString('en-us', {hour: 'numeric', minute: 'numeric'})
+  return `
+    # <a:alert:1116830883991781376> World Boss Alert <a:alert:1116830883991781376>
+    ## ${bossName}
+    Starts at ${formattedTime} EST in ${location}
+    <@&1116831413791105034>
+  `
 }
 
 client.on('ready', () => {
-  console.log('Bot is ready')
-})
+  const alerts = {}
+  const channel = client.channels.cache.get('1086009931364315136')
+  console.log(`Loaded`)
+  setInterval(() => {
+    console.log(`Checking timers`)
+    getBossTimers().then((event) => {
+      if (_.isEmpty(event)) {
+        console.log("no timers found")
+        return
+      }
 
-client.on('messageCreate', message => {
-  if (message.content === '!bosstimer') {
-    getBossTimers().then((timers) => {
-      console.log(timers)
-      message.reply(timers)
+      console.log("found timer")
+      const now = new Date()
+      if (alerts[event.name] && (now - alerts[event.name] < 60 * 60 * 10000) ) return
+
+      console.log(`alerting for event ${event.name} at ${now}`)
+      alerts[event.name] = now
+      channel.send(formatMessage(event))
     })
-  }
+  }, 5 * 60 * 1000) // Every 5 minutes
 })
 
 client.login(process.env.TOKEN)
